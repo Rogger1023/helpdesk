4e9d9f674bfa2aca4c455c78048b0731156e22d3
Rogger <rogger1023@hotmail.com>
2026-09-21 21:58:21 -0300

    feat: configure Vue and Inertia

---
+++ resources/js/Pages/home.vue
@@ -0,0 +1,22 @@
+<script setup>
+const projeto = 'Helpdesk de TI';
+</script>
+
+<template>
+    <main>
+        <h1>{{ projeto }}</h1>
+        <div> 
+            <p>
+                Sistema de Controle de Chamados
+            </p>
+            <p>
+                 Projeto desenvolvido com:
+            </p>
+                <div> Laravel </div>
+                <div> Vue.js </div>
+                <div> Inertia.js </div>
+                <div> MySQL </div>
+                <div> Docker / Laravel Sail </div>         
+         </div>
+    </main>
+</template>
